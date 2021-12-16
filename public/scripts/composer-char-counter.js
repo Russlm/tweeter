dd1ed920fccc23c24a1c35635f0e668d0a98acea
Russell Mair <russell.e.mair@gmail.com>
2021-12-16 06:33:13 +0000

    this commit adds in basic functionality for the tweets container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,20 @@
+
 $(document).ready(function() {
-  console.log('its ready.')
+  let counter = document.getElementById('counter');
+  let textArea = document.getElementById('tweet-text');
+  let maxChars = 140;
+  $("#tweet-text").on('input', function() {
+    let remaining = maxChars - textArea.value.length;
+    if (remaining <= 0) {
+      counter.style.color = "red";
+    } else {
+      counter.style.color = "black";
+    }     counter.textContent = remaining;
+  });
 });
+
+// let remaining = counter - this
+// if (remaining < 140) {
+//   counter.color = 'red';
+// }
+// counter= remaining
