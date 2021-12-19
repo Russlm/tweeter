1741d3ea013a434aa008f4523a4d43b386a5ffa9
Russell Mair <russell.e.mair@gmail.com>
2021-12-19 02:06:05 +0000

    implements working createTweetElement function.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+$(document).ready(function(){
+
+const tweetData = {
+  "user": {
+    "name": "Bob",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+  
+
+
+
+const createTweetElement = (tweet) => {
+  const tweetElement = `
+  <article class= 'tweets'> 
+        <header>
+          <div>
+            <img class= "profilePic"src=${tweet.user.avatars}>
+              <p class= "username">${tweet.user.name}</p>
+            </div>
+          <div>
+            <p class= "handle">${tweet.user.handle}</p></div>
+        </header>
+        <p class= "tweetBody">${escape(tweet.content.text)}</p>
+        <footer>
+          <div>
+            <p>${(tweet.created_at.name)}</p></div>
+          <div>
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+      `
+  return tweetElement;
+}
+
+
+
+  // $("username") = tweet.user.name 
+  // $("profilePic") = tweet.user.avatars 
+  // $("handle") = tweet.user.handle 
+  // $("#tweet-text") = tweet.content.text 
+  // $("timestamp") = tweet.content.text 
+  // $("timestamp") = tweet["created_at"];
+
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('.tweets-container').append($tweet); 
+
+});
