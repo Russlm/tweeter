b42266889b995580cd7470a7abdf7701239a5d8e
Russell Mair <russell.e.mair@gmail.com>
2021-12-19 02:34:01 +0000

    fixes issue with the tweetbody formatting.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,7 +30,7 @@ const createTweetElement = (tweet) => {
           <div>
             <p class= "handle">${tweet.user.handle}</p></div>
         </header>
-        <p class= "tweetBody">${escape(tweet.content.text)}</p>
+        <p class= "tweetBody">${tweet.content.text}</p>
         <footer>
           <div>
             <p>${timeago.format(tweet.created_at.name)}</p></div>
@@ -45,10 +45,50 @@ const createTweetElement = (tweet) => {
   return tweetElement;
 }
 
-const $tweet = createTweetElement(tweetData);
-
 // Test / driver code (temporary)
+const $tweet = createTweetElement(tweetData);
 console.log($tweet); // to see what it looks like
 $('.tweets-container').append($tweet); 
+
+// Render Tweets Driver Code  
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+// const renderTweets = function(tweets) {
+//   // loops through tweets
+//   for(tweet in tweets) {
+//     console.log("tweet in question", tweets[tweet]);
+//     createTweetElement(tweets[tweet]);
+//     console.log(createTweetElement(tweets[tweet]))
+//     $('.tweets-container').append(tweets[tweet]); 
+
+//   }
+//   // calls createTweetElement for each tweet
+//   // takes return value and appends it to the tweets container
+// }
+
+// renderTweets(data);
 
 });
