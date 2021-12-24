726cc51ee32b17230cda34a550cd376849c09fd5
Russell Mair <russell.e.mair@gmail.com>
2021-12-24 02:31:15 +0000

    removes comments from code.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,19 +6,6 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function(){
-
-const tweetData = {
-  "user": {
-    "name": "Bob",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac2"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -51,12 +38,7 @@ const createTweetElement = (tweet) => {
   return tweetElement;
 }
 
-// Test / driver code (temporary)
-const $tweet = createTweetElement(tweetData);
-console.log($tweet); // to see what it looks like
-// $('.tweets-container').append($tweet); 
 
-// Render Tweets Driver Code  
 
 
 const renderTweets = function(tweets) {
