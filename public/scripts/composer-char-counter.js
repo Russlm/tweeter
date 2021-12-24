a4f2b01b990e5ac108a30c80d433d58085101170
Russell Mair <russell.e.mair@gmail.com>
2021-12-24 02:32:23 +0000

    further removes old code.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
-  // let counter = document.getElementById('counter');
-  // let textArea = document.getElementById('tweet-text');
-  // let maxChars = 140;
   let counter = $("#counter");
   
  $("#tweet-text").on('input', function() {
-  //  let remaining = 140 - textArea.value.length;
    let currentLength = $(this).val().length;
    counter.val(140 - currentLength);
    if (currentLength >= 140) {
@@ -13,12 +9,5 @@ $(document).ready(function() {
    } else {
     counter.css("color", "black");
    }
-  //  counter.textContent = remaining;
  });
 });
-
-// let remaining = counter - this
-// if (remaining < 140) {
-//   counter.color = 'red';
-// }
-// counter= remaining
